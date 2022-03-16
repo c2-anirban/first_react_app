05fa407de86a8bb86406034d1757b55a022f45d4
saikat-samanta <saikatsamanta737@gmail.com>
2022-03-16 13:14:59 +0530

    new

--- src/signIn.js
+++ src/signIn.js
@@ -82,7 +82,7 @@ const SignIn = () => {
                     <input
                       type="password"
                       name="password"
-                      minlength="8"
+                      minLength="8"
                       pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
                       title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
                       required
