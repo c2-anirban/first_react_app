f9a492081cef4edc1aaebc396f7521584e18b078
saikat-samanta <saikatsamanta737@gmail.com>
2022-03-17 14:58:12 +0530

    new1

--- src/signUp.jsx
+++ src/signUp.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 function SignUp() {
   return (
-    // test
     <div className="container">
       <div className="card shadow p-8 mb-9 bg-body rounded">
         <div className="card-body">
